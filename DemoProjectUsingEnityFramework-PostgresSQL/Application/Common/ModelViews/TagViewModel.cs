382dd516d373523a25ad7e5d6cf0df4ec95500be
anchalBomjan <anchal.percoidit@gmail.com>
2026-04-05 00:17:36 +0545

    Some changes

--- DemoProjectUsingEnityFramework-PostgresSQL/Application/Common/ModelViews/TagViewModel.cs
+++ DemoProjectUsingEnityFramework-PostgresSQL/Application/Common/ModelViews/TagViewModel.cs
@@ -4,4 +4,5 @@ public class TagViewModel
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public int ProductsCount { get; set; }
 }
--- DemoProjectUsingEnityFramework-PostgresSQL/Application/Common/Responses/Response.cs
+++ DemoProjectUsingEnityFramework-PostgresSQL/Application/Common/Responses/Response.cs
@@ -5,7 +5,10 @@ public class Response<T>
     public bool IsSuccess { get; set; }
     public T? Data { get; set; }
     public string? Message { get; set; }
+    public List<string>? Errors { get; set; }
 
     public static Response<T> Success(T data) => new() { IsSuccess = true, Data = data };
+    public static Response<T> Success(T data, string message) => new() { IsSuccess = true, Data = data, Message = message };
     public static Response<T> Failure(string message) => new() { IsSuccess = false, Message = message };
+    public static Response<T> Failure(string message, List<string> errors) => new() { IsSuccess = false, Message = message, Errors = errors };
 }
